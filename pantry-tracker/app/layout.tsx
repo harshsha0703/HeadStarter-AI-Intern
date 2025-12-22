59e39230fa782a45c37d6029ea14e763ff132b1c
harshsha0703 <harshsha0703@gmail.com>
2025-12-22 14:37:25 -0600

    Create layout.tsx

---
+++ pantry-tracker/app/layout.tsx
@@ -0,0 +1,11 @@
+import { CartProvider } from "@/components/CartContext";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <CartProvider>{children}</CartProvider>
+      </body>
+    </html>
+  );
+}
